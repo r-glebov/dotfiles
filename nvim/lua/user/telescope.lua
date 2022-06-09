4c5e7a610f8e7000e0bfc4820660d028416cb646
Roman Glyebov <rglyebov@atlassian.com>
2022-06-09 15:29:03 +0200

    Telescope config clean up

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -117,23 +117,13 @@ telescope.setup {
       layout_config = {
         width = 0.7,
         height = 0.6,
-        -- preview_width = 0.6,
       },
       path_display = { "truncate" },
     },
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
   },
   extensions = {
-    -- Your extension configuration goes here:
     -- extension_name = {
     --   extension_config_key = value,
     -- }
-    -- please take a look at the readme of the extension you want to configure
   },
 }
