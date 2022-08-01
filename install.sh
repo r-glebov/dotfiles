26c39bbb826e3b5f65ebfb86a6695019d4dd9d78
Roman Glebov <r.gleboff@gmail.com>
2022-08-01 13:57:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ mkdir -p ~/.config/nvim
 ln -sf $dotfiles_dir/nvim ~/.config
 
 # Run nvim command to install plugins
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync'
+#nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync'
 
 # Run nvim command to install Treesitter languages
-nvim --headless -c 'set cmdheight=50' -c 'silent TSInstallSync all' -c 'sleep 20' -c 'qall'
+#nvim --headless -c 'set cmdheight=50' -c 'silent TSInstallSync all' -c 'sleep 20' -c 'qall'
 
