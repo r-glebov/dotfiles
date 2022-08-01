862c6ca4178489bea93a484259b657bcd359e566
Roman Glyebov <rglyebov@atlassian.com>
2022-08-01 15:49:12 +0200

    Fix install.sh for Gitpod

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-dotfiles_dir=/workspaces/.codespaces/.persistedshare/dotfiles/
+# Copy .dotfiles from git repo
+git clone git@github.com:r-glebov/dotfiles.git ~/.dotfiles
+
+dotfiles_dir=~/.dotfiles/
 
 # Symlink the files
 mkdir -p ~/.config/nvim
 ln -sf $dotfiles_dir/nvim ~/.config
 
 # Run nvim command to install plugins
-#nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync'
 
 # Run nvim command to install Treesitter languages
-#nvim --headless -c 'set cmdheight=50' -c 'silent TSInstallSync all' -c 'sleep 20' -c 'qall'
+nvim --headless -c 'set cmdheight=50' -c 'silent TSInstallSync all' -c 'sleep 20' -c 'qall'
 
