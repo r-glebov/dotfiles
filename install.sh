b0453e8e07a34315c5c05ba253cff7b91bd72bfc
Roman Glyebov <rglyebov@atlassian.com>
2022-08-01 17:48:29 +0200

    fix symlink creation

--- install.sh
+++ install.sh
@@ -3,13 +3,12 @@
 # Copy .dotfiles from git repo
 #git clone git@github.com:r-glebov/dotfiles.git ~/.dotfiles
 
-dotfiles_dir=~/.dotfiles/
+dotfiles_dir=~/.dotfiles
 
 # Symlink the files
-mkdir -p ~/.config/nvim
-ln -sf $dotfiles_dir/nvim ~/.config
+ln -sf ~/.dotfiles/nvim ~/.config
 
-cd GITPOD_REPO_ROOTS
+cd $GITPOD_REPO_ROOTS
 
 # Run nvim command to install plugins
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync'
