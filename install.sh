06b79a968e1138f81ae39cf5e8eee2545b44c89b
Roman Glyebov <rglyebov@atlassian.com>
2022-08-01 10:37:27 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ~/
+
+# Copy .dotfiles from git repo
+git clone git@github.com:r-glebov/dotfiles.git
+
+dotfiles_dir=~/dotfiles
+
+# Symlink the files
+ln -sf $dotfiles_dir/nvim ~/.config/nvim
+
+# Run nvim command to install plugins
+RUN nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync'
+
+# Run nvim command to install Treesitter languages
+RUN nvim --headless -c 'set cmdheight=50' -c 'silent TSInstallSync all' -c 'sleep 20' -c 'qall'
+
