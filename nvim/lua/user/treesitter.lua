e36bb9b257bd1768283be2e9737965ff35a2dc28
Roman Glyebov <rglyebov@atlassian.com>
2022-09-01 11:39:28 +0200

    disable norg config

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,26 +1,27 @@
-local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-
--- These two are optional and provide syntax highlighting
--- for Neorg tables and the @document.meta tag
-parser_configs.norg_meta = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-        files = { "src/parser.c" },
-        branch = "main"
-    },
-}
-
-parser_configs.norg_table = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
-        files = { "src/parser.c" },
-        branch = "main"
-    },
-}
+-- local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+--
+-- -- These two are optional and provide syntax highlighting
+-- -- for Neorg tables and the @document.meta tag
+-- parser_configs.norg_meta = {
+--     install_info = {
+--         url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+--         files = { "src/parser.c" },
+--         branch = "main"
+--     },
+-- }
+--
+-- parser_configs.norg_table = {
+--     install_info = {
+--         url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+--         files = { "src/parser.c" },
+--         branch = "main"
+--     },
+-- }
 
 --- Treesitter settings
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "ruby", "lua", "javascript", "norg", "norg_meta", "norg_table" },
+  --ensure_installed = { "ruby", "lua", "javascript", "norg", "norg_meta", "norg_table" },
+  ensure_installed = { "ruby", "lua", "javascript" },
   highlight = {
     enable = true,
     disable = {'org'},
